ae3da91ca4e8e5d9f46c132aa615e78df05ada16
Kuba <pexilver123456@gmail.com>
2026-02-08 09:30:02 +0100

    Remove Haxball server setup and add package.json
    Removed the Haxball room setup and player join announcement. Added package.json configuration for the project.

--- index.js
+++ index.js
@@ -1,22 +1,11 @@
-const HB = require('haxball-headless');
-
-const room = HB.createRoom({
-  roomName: "Mój Serwer 24/7",
-  maxPlayers: 12,
-  public: true
-});
-
-room.setDefaultStadium("Classic");
-room.setScoreLimit(5);
-room.setTimeLimit(0);
-
-room.onPlayerJoin = (player) => {
-  room.sendAnnouncement(
-    "Witaj " + player.name + " na serwerze!",
-    player.id,
-    0x00ff00,
-    "bold"
-  );
-};
-
-console.log("Serwer wystartował! Możesz wejść: https://www.haxball.com/play?c=" + room.getRoomLink());
+{
+  "name": "haxball-server",
+  "version": "1.0.0",
+  "main": "index.js",
+  "scripts": {
+    "start": "node index.js"
+  },
+  "dependencies": {
+    "puppeteer": "^21.3.8"
+  }
+}
