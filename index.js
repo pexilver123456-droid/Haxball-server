fe817bfdf678b34b11cf53a2a6fb233cede04c1c
Kuba <pexilver123456@gmail.com>
2026-02-08 09:20:58 +0100

    Refactor room initialization and player join announcement

--- index.js
+++ index.js
@@ -1,19 +1,22 @@
-const room = HBInit({
-  roomName: "MÓJ SERWER",
+const HB = require('haxball-headless');
+
+const room = HB.createRoom({
+  roomName: "Mój Serwer 24/7",
   maxPlayers: 12,
-  public: true,
-  noPlayer: true
+  public: true
 });
 
 room.setDefaultStadium("Classic");
 room.setScoreLimit(5);
 room.setTimeLimit(0);
 
-room.onPlayerJoin = function(player) {
+room.onPlayerJoin = (player) => {
   room.sendAnnouncement(
-    "Witaj " + player.name,
+    "Witaj " + player.name + " na serwerze!",
     player.id,
     0x00ff00,
     "bold"
   );
 };
+
+console.log("Serwer wystartował! Możesz wejść: https://www.haxball.com/play?c=" + room.getRoomLink());
